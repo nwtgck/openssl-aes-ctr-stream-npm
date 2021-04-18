a79ace36518bc617cfcfdc796da354a04e217810
Ryo Ota <nwtgck@nwtgck.org>
2021-04-18 10:05:16 +0900

    test: encryption and decryption

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,25 @@
-// TODO: You should change this content.
-
 import * as assert from 'power-assert';
+import {aesCtrDecrypt, aesCtrEncrypt} from "../src";
+import {readableStreamToUint8Array} from "binconv";
 
-describe('1 + 1', () => {
-  it('1 should be 1', () => {
-    assert.strictEqual(1, 1);
+describe('aes-ctr', () => {
+  it('should encrypt and decrypt', async () => {
+    const plainReadableStream =  new ReadableStream({
+      start: (controller) => {
+        controller.enqueue(new TextEncoder().encode("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcde"));
+        controller.enqueue(new TextEncoder().encode("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCD"));
+        controller.enqueue(new TextEncoder().encode("01234567890123456789012345678901234567890123"));
+        controller.close();
+      }
+    });
+    const password = "1234";
+    const pbkdf2Options = {
+      iterations: 100000,
+      hash: "SHA-256",
+    } as const;
+    const encryptedReadableStream = aesCtrEncrypt(plainReadableStream, password, pbkdf2Options);
+    const decryptedReadableStream = aesCtrDecrypt(encryptedReadableStream, password, pbkdf2Options)
+    const decryptedText: string = new TextDecoder().decode(await readableStreamToUint8Array(decryptedReadableStream));
+    assert.strictEqual(decryptedText, 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdeABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCD01234567890123456789012345678901234567890123');
   });
 });
