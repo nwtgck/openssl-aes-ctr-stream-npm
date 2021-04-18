0e9d96f15fb7949473827f905686f52efcbf6ee6
Ryo Ota <nwtgck@nwtgck.org>
2021-04-18 10:04:59 +0900

    implement OpenSSL-compatible AES CTR decryption

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {ReadableStreamSizedReader} from "readable-stream-sized-reader";
-import {mergeUint8Arrays} from "binconv";
+import {mergeUint8Arrays} from "binconv/dist/src/mergeUint8Arrays";
 
 const keyBitSize = 256;
 const ivBitSize = 128;
@@ -67,4 +67,43 @@ export function aesCtrEncrypt(readableStream: ReadableStream<Uint8Array>, passwo
       incrementCounter(counter);
     }
   });
+}
+
+function throwIfUnexpectedLength(result: ReadableStreamDefaultReadResult<Uint8Array>, expectedByteLength: number): asserts result is ReadableStreamDefaultReadValueResult<Uint8Array> {
+  if (result.done) {
+    throw new Error("unexpected done");
+  }
+  if (result.value.byteLength !== expectedByteLength) {
+    throw new Error(`expected ${expectedByteLength}, but ${result.value.byteLength}`);
+  }
+}
+
+export function aesCtrDecrypt(readableStream: ReadableStream<Uint8Array>, password: string, pbkdf2Options: { iterations: number, hash: "SHA-256" } ): ReadableStream<Uint8Array> {
+  const reader = new ReadableStreamSizedReader(readableStream.getReader());
+  let salt: Uint8Array;
+  let cryptoKey: CryptoKey;
+  let counter: Uint8Array;
+  return new ReadableStream({
+    async start(ctrl) {
+      const Salted__ = await reader.read(8);
+      throwIfUnexpectedLength(Salted__, 8);
+      const saltResult = await reader.read(8);
+      throwIfUnexpectedLength(saltResult, 8);
+      salt = saltResult.value;
+      const keyAndIv = await deriveKeyAndIvByPbkdf2(salt, password, pbkdf2Options);
+      cryptoKey = keyAndIv.cryptoKey;
+      counter = keyAndIv.iv;
+    },
+    async pull(ctrl) {
+      const result = await reader.read(blockByteSize);
+      if (result.done) {
+        ctrl.close();
+        return;
+      }
+      const decrypted = await crypto.subtle.decrypt({ name: "AES-CTR", counter, length: ivBitSize }, cryptoKey, result.value);
+      ctrl.enqueue(new Uint8Array(decrypted));
+      // Increment counter destructively
+      incrementCounter(counter);
+    }
+  })
 }
